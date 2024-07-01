6046467fc27c9fa8108578e24d63a743d41e475f
Mirko <mirko@pop-os.localdomain>
2024-06-30 17:58:28 -0700

    wip: add very simple transformer

---
+++ models.py
@@ -0,0 +1,65 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+class BigramModel(nn.Module):
+    
+    def __init__(self, vocab_size):
+        super().__init__()
+        self.embedding = nn.Embedding(vocab_size, vocab_size)
+        
+    def forward(self, x):
+        logits = self.embedding(x)
+        return logits
+    
+    def generate(self, idx, max_length):
+        # idx is a tensor of indexes with shape (B, T)
+        assert max_length > 0, "max_length must be at least 1"
+        # The bigram does not use the context, just the last characters of the sequence.
+        # Because of that we can use idx[:, -1]
+        sampled = idx[:,-1] # (B)
+        out = []
+        while max_length > 0:
+            logits = self.forward(sampled) # (B, C)
+            probs = F.softmax(logits, dim=-1) # (B, C)
+            sampled = torch.multinomial(probs, 1, replacement=True) # (B, 1)
+            sampled = sampled.flatten() # (B)
+            out.append(sampled)
+            max_length -= 1
+            
+        return torch.cat((idx, torch.stack(out, dim=1)), dim=1)
+    
+class Transformer(nn.Module):
+
+    def __init__(self, vocab_size: int, hidden_size: int):
+        super().__init__()
+        self.vocab_size = vocab_size
+        self.hidden_size = hidden_size
+
+        self.pos_embedding = nn.Embedding(self.vocab_size, self.hidden_size)
+        self.embedding = nn.Embedding(self.vocab_size, self.hidden_size)
+
+        self.weights_k = torch.nn.Linear(self.hidden_size, self.hidden_size, bias=False)
+        self.weights_q = torch.nn.Linear(self.hidden_size, self.hidden_size, bias=False)
+        self.weights_v = torch.nn.Linear(self.hidden_size, self.hidden_size, bias=False)
+
+        self.weights_o = torch.nn.Linear(self.hidden_size, self.vocab_size)
+
+    def forward(self, x):
+        # x: (B, T)
+        _, seq_length = x.shape
+        tril = torch.tril(torch.ones((seq_length, seq_length)))
+        x = self.embedding(x) # (B, T, H)
+        x += self.pos_embedding(torch.arange(0, seq_length)) # (B, T, H)
+
+        k = self.weights_k(x) # (B, T, H)
+        q = self.weights_q(x) # (B, T, H)
+        v = self.weights_v(x) # (B, T, H)
+
+        z = k @ q.transpose(1,2) # (B, T, T) the attention matrix
+        z = tril * z # (B, T, T) mask out non-causal (q, k) pairs
+        z = z @ v # (B, T, H) the values
+        z = z + x # (B, T, H) residual connection
+        z = self.weights_o(z) # (B, T, V) where V is vocab_size, logits
+
+        return z
--- train_transformer.py
+++ train_transformer.py
@@ -1,12 +1,12 @@
 import torch
 from utils import TextData, train
-from models import BigramModel
+from models import Transformer
 
 if __name__ == "__main__":
     torch.manual_seed(4331)
     train_data = TextData("shakespeare.txt", 8, True, train_frac=0.9)
     val_data = TextData("shakespeare.txt", 8, False, train_frac=0.9)
-    model = BigramModel(train_data.vocab_size)
+    model = Transformer(train_data.vocab_size, 32)
     optimizer = torch.optim.Adam(model.parameters())
 
-    train(model, optimizer, train_data, val_data, 32, 10000, 1000, 1000)
+    train(model, optimizer, train_data, val_data, 32, 50000, 1000, 1000)
